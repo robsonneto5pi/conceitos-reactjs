26e1d7cf0416842ede3299755f3e786c9a2c1351
robsonneto5 <robsonneto5@gmail.com>
2021-06-30 17:27:42 -0300

    fixed different command

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     await api.delete(`repositories/${id}`)
 
     setRepositories(repositories.filter(
-      repository => repository.id != id
+      repository => repository.id !== id
     ))
   }
 
